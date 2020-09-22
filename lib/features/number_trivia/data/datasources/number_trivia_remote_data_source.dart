b05f19d9217ebab45be10b5eb68465b15307bab0
Rahul_Vijay <vk.rahul318@gmail.com>
2020-09-22 16:54:01 +0530

    mock GET

--- lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -2,19 +2,19 @@ import 'package:capp/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:meta/meta.dart';
 import 'package:http/http.dart' as http;
 
-abstract class NumberTriviaRemoteDataSource{
+abstract class NumberTriviaRemoteDataSource {
   Future<NumberTrivia> getConcreteNumberTrivia(int number);
   Future<NumberTrivia> getRandomNumberTrivia();
 }
 
-class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource{
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
   final http.Client client;
 
   NumberTriviaRemoteDataSourceImpl({@required this.client});
   @override
   Future<NumberTrivia> getConcreteNumberTrivia(int number) {
-    // TODO: implement getConcreteNumberTrivia
-    throw UnimplementedError();
+    client.get('http://numbersapi.com/$number',
+        headers: {'Content-type': 'application/json'});
   }
 
   @override
--- test/features/data/datasources/number_trivia_remote_data_source_test.dart
+++ test/features/data/datasources/number_trivia_remote_data_source_test.dart
@@ -3,16 +3,36 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/mockito.dart';
 
+import '../../../fixtures/fixture_reader.dart';
+
 /// Mocking the http client package
 
-class MockHttpClient extends Mock implements http.Client{}
+class MockHttpClient extends Mock implements http.Client {}
 
-void main(){
+void main() {
   NumberTriviaRemoteDataSourceImpl dataSource;
   MockHttpClient mockHttpClient;
 
-  setUp((){
+  setUp(() {
     mockHttpClient = MockHttpClient();
     dataSource = NumberTriviaRemoteDataSourceImpl(client: mockHttpClient);
+  });
+
+  group("getConcreteNumberTrivia", () {
+    final tNumber = 1;
+
+    test("should perform a GET request on a URL with number being the endpoint",
+        () {
+      // arrange
+      when(mockHttpClient.get(any, headers: anyNamed('headers')))
+          .thenAnswer((_) async => http.Response(fixture('trivia.json'), 200));
+      // act
+      dataSource.getConcreteNumberTrivia(tNumber);
+      // assert
+      verify(mockHttpClient.get(
+        'http://numbersapi.com/$tNumber',
+        headers: {'Content-type': 'application/json'},
+      ));
+    });
   });
 }
