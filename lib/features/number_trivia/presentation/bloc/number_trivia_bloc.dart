79945fd5844242fc52b2039b75512fa620cce6c9
Rahul_Vijay <vk.rahul318@gmail.com>
2020-09-22 21:13:29 +0530

    setup events

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
--- lib/features/number_trivia/presentation/bloc/number_trivia_event.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_event.dart
@@ -1,4 +1,20 @@
 part of 'number_trivia_bloc.dart';
+
+@immutable
 abstract class NumberTriviaEvent extends Equatable {
   const NumberTriviaEvent();
+}
+
+class GetTriviaForConcreteNumber extends NumberTriviaEvent{
+  final String numberString;
+
+  GetTriviaForConcreteNumber(this.numberString);
+  @override
+  List<Object> get props => [numberString];
+}
+
+class GetTriviaForRandomNumber extends NumberTriviaEvent{
+  @override
+  // TODO: implement props
+  List<Object> get props => throw UnimplementedError();
 }
