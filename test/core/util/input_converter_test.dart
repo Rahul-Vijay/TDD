81118f8cb4f9e9779f7e4db3e3912f78ba938b4b
Rahul_Vijay <vk.rahul318@gmail.com>
2020-09-22 21:22:08 +0530

    test for non int string

--- test/core/util/input_converter_test.dart
+++ test/core/util/input_converter_test.dart
@@ -18,5 +18,14 @@ void main(){
       // assert
       expect(result, Right(123));
     });
+
+    test("should return a failure when the string is not an integer", () async {
+      // arrange
+      final str = 'abc';
+      // act
+      final result = inputConverter.stringToUnsignedInteger(str);
+      // assert
+      expect(result, Left(InvalidInputFailure()));
+    });
   });
 }
