0ecb09cacbbb210510debc3b0b957bd6217df74e
Rahul_Vijay <vk.rahul318@gmail.com>
2020-09-22 21:17:09 +0530

    input converter class

---
+++ lib/core/util/input_converter.dart
@@ -0,0 +1,10 @@
+import 'package:capp/core/error/failures.dart';
+import 'package:dartz/dartz.dart';
+
+class InputConverter {
+  Either<Failure, int> stringToUnsignedInteger(String str){
+    // TODO: Implement 
+  }
+}
+
+class InvalidInputFailure extends Failure{}
