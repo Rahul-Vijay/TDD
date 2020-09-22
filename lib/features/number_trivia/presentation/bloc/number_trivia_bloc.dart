9584a8b45b5713a68a1a0341a19c765c29a05280
Rahul_Vijay <vk.rahul318@gmail.com>
2020-09-22 21:28:26 +0530

    setup states

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
+import 'package:capp/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 part 'number_trivia_event.dart';
