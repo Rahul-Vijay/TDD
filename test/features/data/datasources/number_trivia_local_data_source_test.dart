db8cc3727d9f48c3b23698ccadb65bcb4d7471ca
Rahul_Vijay <vk.rahul318@gmail.com>
2020-09-22 14:39:29 +0530

    add test

--- test/features/data/datasources/number_trivia_local_data_source_test.dart
+++ test/features/data/datasources/number_trivia_local_data_source_test.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:capp/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:capp/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../fixtures/fixture_reader.dart';
 
 class MockSharedPreferences extends Mock implements SharedPreferences{}
 
@@ -14,5 +19,20 @@ void main(){
     dataSource = NumberTriviaLocalDataSourceImpl(
       sharedPreferences: mockSharedPreferences
     );
+  });
+
+  group('getLastNumberTrivia', (){
+    final tNumberTriviaModel = NumberTriviaModel.fromJson(json.decode(fixture('trivia_cached.json')));
+
+    test('should return NumberTrivia from SharedPreferences when there is one in cache', () async {
+      // arrange
+      when(mockSharedPreferences.getString(any))
+        .thenReturn(fixture('trivia_cached.json'));
+      // act
+      final result = await dataSource.getLastNumberTrivia();
+      // assert
+      verify(mockSharedPreferences.getString('CACHED_NUMBER_TRIVIA'));
+      expect(result, equals(tNumberTriviaModel));
+    });
   });
 }
