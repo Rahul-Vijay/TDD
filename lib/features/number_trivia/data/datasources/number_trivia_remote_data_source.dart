e1a7db5caca926edde86530cf632cd95b1735ec1
Rahul_Vijay <vk.rahul318@gmail.com>
2020-09-22 17:05:39 +0530

    test for 404 response

--- lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:capp/core/error/exception.dart';
 import 'package:capp/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:capp/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:meta/meta.dart';
@@ -16,9 +17,12 @@ class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
   NumberTriviaRemoteDataSourceImpl({@required this.client});
   @override
   Future<NumberTriviaModel> getConcreteNumberTrivia(int number) async {
-    final jsonString = await client.get('http://numbersapi.com/$number',
+    final response = await client.get('http://numbersapi.com/$number',
         headers: {'Content-type': 'application/json'});
-    return NumberTriviaModel.fromJson(json.decode(jsonString.body));
+    if (response.statusCode == 200)
+      return NumberTriviaModel.fromJson(json.decode(response.body));
+    else
+      throw ServerException();
   }
 
   @override
--- test/features/data/datasources/number_trivia_remote_data_source_test.dart
+++ test/features/data/datasources/number_trivia_remote_data_source_test.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
+import 'package:capp/core/error/exception.dart';
 import 'package:capp/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:capp/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:matcher/matcher.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/mockito.dart';
@@ -38,10 +40,10 @@ void main() {
       ));
     });
 
-    final tNumberTriviaModel = NumberTriviaModel.fromJson(json.decode(fixture('trivia.json')));
+    final tNumberTriviaModel =
+        NumberTriviaModel.fromJson(json.decode(fixture('trivia.json')));
 
-    test("should return a NumberTrivia Object on 200(success)",
-        () async {
+    test("should return a NumberTrivia Object on 200(success)", () async {
       // arrange
       when(mockHttpClient.get(any, headers: anyNamed('headers')))
           .thenAnswer((_) async => http.Response(fixture('trivia.json'), 200));
@@ -49,6 +51,16 @@ void main() {
       final result = await dataSource.getConcreteNumberTrivia(tNumber);
       // assert
       expect(result, equals(tNumberTriviaModel));
+    });
+
+    test("should throw a ServerException on 404 response(Not found)", () async {
+      // arrange
+      when(mockHttpClient.get(any, headers: anyNamed('headers')))
+          .thenAnswer((_) async => http.Response('Something went wrong', 404));
+      // act
+      final call = dataSource.getConcreteNumberTrivia;
+      // assert
+      expect(() => call(tNumber), throwsA(TypeMatcher<ServerException>()));
     });
   });
 }
