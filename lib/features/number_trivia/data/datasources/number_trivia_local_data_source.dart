0fe275b2e7f18555a95cc7e54df385c95656a7e1
Rahul_Vijay <vk.rahul318@gmail.com>
2020-09-22 14:32:40 +0530

    implement abstract class and add test

--- lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
@@ -1,6 +1,25 @@
 import 'package:capp/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:meta/meta.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class NumberTriviaLocalDataSource{
   Future<NumberTriviaModel> getLastNumberTrivia();
   Future<void> cacheNumberTrivia(NumberTriviaModel triviaModel);
+}
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource{
+  final SharedPreferences sharedPreferences;
+
+  NumberTriviaLocalDataSourceImpl({@required this.sharedPreferences});
+  @override
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaModel) {
+    // TODO: implement cacheNumberTrivia
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() {
+    // TODO: implement getLastNumberTrivia
+    throw UnimplementedError();
+  }
 }
